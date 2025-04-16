31484e5c881bccb243b4fb81adbe8a4146e84a1d
italoalcantaraa <italoalcantaraa6@gmail.com>
2025-04-15 22:08:40 -0300

    ajustes

---
+++ src/pages/customer/login/login.jsx
@@ -0,0 +1,16 @@
+export default function login(){
+
+
+    return(
+        <main>
+            <nav>
+                <figure><img src="" alt="" /></figure>
+
+                <h5><strong>Login into your account</strong></h5>
+            </nav>
+            <aside>
+
+            </aside>
+        </main>
+    )
+}
