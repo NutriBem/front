cae5ddb3608fafce2ee47aa83f8139a69e5415f3
Alnfire <alisson.s28356@gmail.com>
2025-05-02 17:18:49 -0300

    Inicio da Home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,16 @@
+import './home.css';
+
+import nutriBem from '../../../public/logoNutriBem.svg';
+
 export default function Home() {
     return(
-        <div>
-            <h1>Home</h1>
+        <div className="container">
+            <div className="header">
+                <div>
+                    <img src={nutriBem} alt="" />
+                    <h1>NUTRIBEM</h1>
+                </div>
+            </div>
         </div>
     );
 }
