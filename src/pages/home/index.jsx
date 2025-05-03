74c9a6a661e83f9ccfd98068f005d24bf5d3db99
Alnfire <alisson.s28356@gmail.com>
2025-05-02 22:22:25 -0300

    Iniciando a home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,11 +4,14 @@ import nutriBem from '../../../public/logoNutriBem.svg';
 
 export default function Home() {
     return(
-        <div className="container">
+        <div className="container_home">
             <div className="header">
-                <div>
+                <div className='logo'>
                     <img src={nutriBem} alt="" />
                     <h1>NUTRIBEM</h1>
+                </div>
+                <div className='icon'>
+                    
                 </div>
             </div>
         </div>
