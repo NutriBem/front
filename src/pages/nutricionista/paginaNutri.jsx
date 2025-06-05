93b6ee38ccf743c062213f8b2b2a3035a14c1500
RodrigoDig <rodrigogomesgoncalves5367@gmail.com>
2025-06-05 18:35:53 -0300

    Tela Nutricionista Refeita

--- src/pages/nutricionista/paginaNutri.jsx
+++ src/pages/nutricionista/paginaNutri.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
-import './paginaNutri.css'; 
+import './paginaNutri.css';
+import { logos } from '../../config/assets';
 
 function Agendamentos() {
   return (
-    <div className="container">
-      <aside className="sidebar">
-        <div className="profile-placeholder"></div>
-      </aside>
+    <div className="nutri-container">
+      <header className="nutri-header">
+        <img src={logos.nutriBem} alt="Nutribem" className="nutri-logo" />
+        <p className="titulo">NUTRIBEM</p>
+      </header>
+      
+      <div className="nutri-title-bar">
+          <h2 className="nutri-title">Agendamentos</h2>
+      </div>
 
-      <main className="main-content">
-        <h1>Agendamentos</h1>
+      <main className="nutri-main">
+        
 
-        <div className="filters">
-          <input type="text" placeholder="Escreva o nome de um paciente" />
-          <select name="status">
-            <option value="">Status</option>
-            <option value="agendada">Agendada</option>
-            <option value="realizada">Realizada</option>
-          </select>
-          <input type="date" />
+        <div className="nutri-filters">
+          <input type="text" placeholder="Nome" className="nutri-input" />
+          <input type="date" className="nutri-input" />
         </div>
 
-        <table>
+        <table className="nutri-table">
           <thead>
             <tr>
-              <th>Nº</th>
               <th>Nome</th>
-              <th>Status</th>
+              <th>Telefone</th>
               <th>Data</th>
               <th>Detalhes</th>
             </tr>
           </thead>
           <tbody>
-            {/* Dados do banco aqui */}
+            <tr>
+              <td>Rodrigo Gomes</td>
+              <td>11999999999</td>
+              <td>05/06/2024</td>
+              <td>
+                <button className="nutri-button">Detalhes</button>
+              </td>
+            </tr>
           </tbody>
         </table>
       </main>
