fb5fae3640e66b9b01a0a8026bc4edff7992efdb
italoalcantaraa <italoalcantaraa6@gmail.com>
2025-06-08 18:32:14 -0300

    path appointment and agenda

--- src/connection/ApiService.js
+++ src/connection/ApiService.js
@@ -2,15 +2,16 @@ import * as patient from './patientPaths'
 import * as person from './personPaths'
 import * as recepcionist from './recepcionistPaths'
 import * as nutricionist from './nutritionistPaths'
-
+import * as appointment from './appointmentPaths'
+import * as agenda from './AgendaPaths'
 // Objeto que centraliza todos os serviços
 const ApiService = {
     person,
     patient,
-    recepcionist,
-    nutricionist
-    //Appointment
-    //agenda
+    recepcionist,   
+    nutricionist,
+    appointment,
+    agenda
 }
 
 export default ApiService;
---
+++ src/connection/appointmentPaths.js
@@ -0,0 +1,60 @@
+import api from "../config/axiosConfig";
+
+export const create = async (patientId, agendaId, receptionistId) => {
+    try {
+        const response = await api.post('/appointment', {
+            patientId,
+            agendaId,
+            receptionistId
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error('[API] Erro no register:', {
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
+        throw error
+    }
+}
+
+export const deleteById = async (id) => {
+    try {
+        const response = await api.delete(`/appointment/${id}`);
+        return response;
+    } catch (error) {
+        console.error('[API] Consulta não encontrada:', {
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
+        throw error
+    }
+}
+
+export const getById = async (id) => {
+    try {
+        const response = await api.get(`/appointment/${id}`);
+        return response;
+    } catch (error) {
+        console.error('[API] Consulta não encontrada:', {
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
+        throw error
+    }
+}
+
+export const getAll = async () => {
+    try {
+        const response = await api.get(`/appointment`);
+        return response;
+    } catch (error) {
+        console.error('[API] Erro', {
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
+        throw error
+    }
+}
+
+
