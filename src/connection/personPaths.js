9456996f19c5f51ceb5522e8afb4b5ed60214707
italoalcantaraa <italoalcantaraa6@gmail.com>
2025-06-10 00:54:22 -0300

    imagem do paciente, ok

--- src/connection/personPaths.js
+++ src/connection/personPaths.js
@@ -1,19 +1,19 @@
 import api from "../config/axiosConfig";
 
-export const getById = async(id) => {
+export const getById = async (id) => {
     try {
         const response = await api.get(`id/${id}`);
         return response.data
     } catch (error) {
         console.error('[API] Erro em achar usuario por id:', {
-        Details: error.response?.data,
-        Status: error.response?.status
-    });
-    throw error
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
+        throw error
     }
 }
 
-export const loginUser = async(email, password) => {
+export const loginUser = async (email, password) => {
     try {
         const response = api.post('/login', {
             email,
@@ -22,27 +22,27 @@ export const loginUser = async(email, password) => {
         return response.data;
     } catch (error) {
         console.error('[API] Erro no login de usuario:', {
-        Details: error.response?.data,
-        Status: error.response?.status
-    });
-    throw error
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
+        throw error
     }
 };
 
-export const deleteById = async(id) => {
+export const deleteById = async (id) => {
     try {
         const response = await api.delete(`delete/${id}`);
         return response.data;
     } catch (error) {
         console.error('[API] Erro em deletar usuario:', {
-        Details: error.response?.data,
-        Status: error.response?.status
-    });
-    throw error
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
+        throw error
     }
 }
 
-export const editUser = async(nome, email, telephone) => {
+export const editUser = async (nome, email, telephone) => {
     try {
         const response = await api.put('/edit', {
             nome,
@@ -52,13 +52,13 @@ export const editUser = async(nome, email, telephone) => {
         return response.data;
     } catch (error) {
         console.error('[API] Erro em editar dados do usuario:', {
-        Details: error.response?.data,
-        Status: error.response?.status
-    });
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
     }
 }
 
-export const editPasswordUser = async(id, password) => {
+export const editPasswordUser = async (id, password) => {
     try {
         const response = await api.put(`/${id}/ChangePassword`, {
             password
@@ -66,8 +66,44 @@ export const editPasswordUser = async(id, password) => {
         return response.data;
     } catch (error) {
         console.error('[API] Erro em editar senha de usuario:', {
-        Details: error.response?.data,
-        Status: error.response?.status
-    });
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
+    }
+}
+
+export const addImagePerson = async (file, id) => {
+    try {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        const response = await api.post(`/image/${id}`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error('[API] Erro em inserir a imagem do usuario:', {
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
+    }
+}
+
+export const getImagePerson = async (id) => {
+    try {
+        const response = await api.get(`/image/search/${id}`, {
+            responseType: 'blob'
+        });
+
+        const blob = URL.createObjectURL(response.data);
+        return blob;
+    } catch (error) {   
+        console.error('[API] Erro em inserir a imagem do usuario:', {
+            Details: error.response?.data,
+            Status: error.response?.status
+        });
     }
 }
