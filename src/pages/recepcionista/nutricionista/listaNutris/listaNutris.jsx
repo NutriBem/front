48e974cd4a8934e4ded0e02a0c63e5eb7c2675de
louiemoreira76 <luis010101work@outlook.com>
2025-06-11 10:52:23 -0300

    Edição perfil completa

--- src/pages/recepcionista/nutricionista/listaNutris/listaNutris.jsx
+++ src/pages/recepcionista/nutricionista/listaNutris/listaNutris.jsx
@@ -1,89 +1,110 @@
-import React, { useState, useEffect } from 'react';
-import './listaNutris.css';
-import Header from '../../../../components/compRecepcionista/compRecepcionista';
-import ApiService from '../../../../connection/ApiService';
+import React, { useState, useEffect } from "react";
+import "./listaNutris.css";
+import Header from "../../../../components/compRecepcionista/compRecepcionista";
+import ApiService from "../../../../connection/ApiService";
+import { removerPerson } from "../../../../utils/removerPerson";
 
 function ListaNutricionistas() {
+  const [nutricionistas, setNutricionistas] = useState([]);
+  const [searchCrm, setSearchCrm] = useState("");
 
-    const [nutricionistas, setNutricionistas] = useState([])
-    const [searchCrm, setSearchCrm] = useState('')
+  //carregar nutri
+  useEffect(() => {
+    async function fetchNutricionistas() {
+      try {
+        const response = await ApiService.nutricionist.GetAllNutritionists();
 
-    //carregar nutri
-    useEffect(() => {
-  async function fetchNutricionistas() {
-    try {
-      const response = await ApiService.nutricionist.GetAllNutritionists()
-
-      console.log("Resposta completa:", response); 
-      setNutricionistas(response)
-
-    } catch (error) {
-      console.error('Erro: ' + error)
+        console.log("Resposta completa:", response);
+        setNutricionistas(response);
+      } catch (error) {
+        console.error("Erro: " + error);
+      }
     }
-  }
-  fetchNutricionistas();
-}, []);
+    fetchNutricionistas();
+  }, []);
 
-const filtraNutricionistas = nutricionistas.filter(nutricionista =>
-    nutricionista.crm && nutricionista.crm.includes(searchCrm)
+  const filtraNutricionistas = nutricionistas.filter(
+    (nutricionista) =>
+      nutricionista.crm && nutricionista.crm.includes(searchCrm)
   );
 
+  return (
+    <div className="container-listaNutri">
+      <Header />
+      <main className="conteudo-principal-listaNutri">
+        <div className="cartao-formulario-listaNutri">
+          <h2 className="titulo-formulario-listaNutri">
+            Lista de Nutricionistas
+          </h2>
 
-
-    return (
-        <div className="container-listaNutri">
-            <Header />
-            <main className="conteudo-principal-listaNutri">
-
-                <div className="cartao-formulario-listaNutri">
-                    <h2 className="titulo-formulario-listaNutri">Lista de Nutricionistas</h2>
-
-                    <div className="barra-pesquisa-listaNutri">
-                        <input
-                            type="text"
-                            placeholder="Informe o nome da Nutricionista"
-                            className="campo-input-listaNutri"
-                             value={searchCrm}
+          <div className="barra-pesquisa-listaNutri">
+            <input
+              type="text"
+              placeholder="Informe o CRM da Nutricionista"
+              className="campo-input-listaNutri"
+              value={searchCrm}
               onChange={(e) => setSearchCrm(e.target.value)}
-                        />
-                        <button className="botao-pesquisa-listaNutri">🔍</button>
-                    </div>
+            />
+            <button className="botao-pesquisa-listaNutri">🔍</button>
+          </div>
 
-                    <div className="tabela-container-listaNutri">
-                        <table className="tabela-listaNutri">
-                            <thead>
-                                <tr>
-                                    <th>NOME</th>
-                                    <th>TELEFONE</th>
-                                    <th>CRM</th>
-                                    <th>EMAIL</th>
-                                    <th>DETALHES</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {filtraNutricionistas.map((nutricionista) => (
-                                      <tr key={nutricionista.id}>
-                    <td>{nutricionista.name || 'N/A'}</td>
-                    <td>{nutricionista.telephone || 'N/A'}</td>
-                    <td>{nutricionista.crm || 'N/A'}</td>
-                    <td>{nutricionista.email || 'N/A'}</td>
+          <div className="tabela-container-listaNutri">
+            <table className="tabela-listaNutri">
+              <thead>
+                <tr>
+                  <th>NOME</th>
+                  <th>TELEFONE</th>
+                  <th>CRM</th>
+                  <th>EMAIL</th>
+                  <th>DETALHES</th>
+                  <th>AÇÕES</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filtraNutricionistas.map((nutricionista) => (
+                  <tr key={nutricionista.crm}>
+                    <td>{nutricionista.name || "N/A"}</td>
+                    <td>{nutricionista.telephone || "N/A"}</td>
+                    <td>{nutricionista.crm || "N/A"}</td>
+                    <td>{nutricionista.email || "N/A"}</td>
                     <td>
-                      <button 
+                      <button
                         className="details-button"
                         onClick={() => console.log("Detalhes:", nutricionista)}
                       >
                         Ver
                       </button>
                     </td>
+                    <td>
+                      <>
+                        <button
+                          className="delete-button"
+                          onClick={() => {
+                            console.log(
+                              "Tentando remover nutricionista com ID:",
+                              nutricionista.id
+                            );
+                            removerPerson(
+                              nutricionista.crm,
+                              nutricionista.name,
+                              ApiService.nutricionist.GetAllNutritionists,
+                              setNutricionistas
+                            );
+                          }}
+                        >
+                          Excluir
+                        </button>
+                      </>
+                    </td>
                   </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </main>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
-};
+      </main>
+    </div>
+  );
+}
 
 export default ListaNutricionistas;
--- src/utils/removerPerson.js
+++ src/utils/removerPerson.js
@@ -3,32 +3,42 @@ import 'react-confirm-alert/src/react-confirm-alert.css';
 import { toast } from 'react-toastify';
 import ApiService from '../connection/ApiService';
 
-export async function removerPerson(id, nome) {
-    confirmAlert({
-        title: 'Remover Usuario',
-      message: `Você tem certeza que deseja remover o usuario ${nome}?`,
-      buttons:[
-        {
-            label: 'Sim',
-            onClick: async () => {
-            try {
-              await ApiService.person.deleteById(id);//get id para pegar o tipo ApiService.person ou ApiService.nutri
-              toast.success("Usuario removido com sucesso!");
-              
-              // Atualiza a lista de usuaris dps remover
-              const response = await ApiService.patient.getAllPatients(); // trocar swifit se for nutri ou pacient
-              setPacientes(response);
-              
-            } catch (error) {
-              console.error("Erro ao remover usuario:", error);
-              toast.error("Erro ao remover usuario!");
+export async function removerPerson(id, nome, fetchFunction, setStateFunction) {
+  confirmAlert({
+    title: 'Remover Usuario',
+    message: `Você tem certeza que deseja remover o usuario ${nome}?`,
+    buttons: [
+      {
+        label: 'Sim',
+        onClick: async () => {
+          try {
+            await ApiService.person.deleteById(id);//get id para pegar o tipo ApiService.person ou ApiService.nutri
+            toast.success("Usuario removido com sucesso!");
+
+            // Atualiza a lista de usuaris dps remover passada como parâmetro
+            const response = await fetchFunction();
+            setStateFunction(response);
+
+          } catch (error) {
+            console.error("Erro ao remover usuário:", error);
+            let errorMessage = "Erro ao remover usuário!";
+
+            if (error.response) {
+              errorMessage = error.response.data?.message ||
+                error.response.data?.error ||
+                JSON.stringify(error.response.data);
+            } else if (error.message) {
+              errorMessage = error.message;
             }
-            }
-        },
-        {
-          label: 'Não',
-          onClick: () => {}
+
+            toast.error(errorMessage);
+          }
         }
-      ]
-    })
+      },
+      {
+        label: 'Não',
+        onClick: () => { }
+      }
+    ]
+  })
 }
